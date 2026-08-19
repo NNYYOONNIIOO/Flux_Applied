075024a9d077a08aaf585f37b0908ca99fb59b9f
NNYYOONNIIOO <wubidaxiang@163.com>
2026-08-19 17:41:49 +0800

    chore: bump version to 1.2.3

--- src/main/java/com/flux_applied/FluxApplied.java
+++ src/main/java/com/flux_applied/FluxApplied.java
@@ -51,7 +51,7 @@ public class FluxApplied implements ILateMixinLoader
 {
     public static final String MODID = "flux_applied";
     public static final String NAME = "Flux Applied";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.2.3";
 
     @SidedProxy(clientSide = "com.flux_applied.ClientProxy", serverSide = "com.flux_applied.CommonProxy")
     public static CommonProxy proxy;
